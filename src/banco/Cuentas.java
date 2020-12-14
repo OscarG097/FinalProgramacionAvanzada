f300788ebe063aedcadecc987f504f61d9909d53
Lucas Basualdo <basualdo1995@gmail.com>
2020-12-14 16:53:06 -0300

    Se crea la clases Cuentas.java para ir creando cuentas de clientes con su usuario/contraseña y saldo

---
+++ src/banco/Cuentas.java
@@ -0,0 +1,43 @@
+package banco;
+
+public class Cuentas {
+
+	private int numCliente;
+	private int nip;
+	private int usuario;
+	private int contrasena;
+	public double saldo;
+	
+	
+	public Cuentas(int numCliente,int nip,double saldo) {
+		usuario=numCliente;
+		contrasena=nip;
+		this.saldo=saldo;
+	}
+	public int getNumCliente() {
+		return numCliente;
+	}
+	public void setNumCliente(int numCliente) {
+		this.numCliente = numCliente;
+	}
+	public int getNip() {
+		return nip;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo=saldo;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public int getUsuario() {
+		return usuario;
+	}
+	public int getContrasena() {
+		return contrasena;
+	}
+
+	
+	
+	
+	
+}
