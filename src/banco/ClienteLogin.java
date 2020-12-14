f1ce76820165f82e7f7c2028f013981c21d36c25
Lucas Basualdo <basualdo1995@gmail.com>
2020-12-14 16:53:36 -0300

    Minimas modificaciones a ClienteLogin

--- src/banco/ClienteLogin.java
+++ src/banco/ClienteLogin.java
@@ -1,16 +1,21 @@
 package banco;
 
-public class ClienteLogin{
+
+
+public class ClienteLogin {
 	
 	static final int[] numCliente = {12345, 54321, 77777, 00000};
 	static final int[] nip = {11111, 22222, 12345, 00000};
 	private int usuario;
 	private int contrasena;
 
+	
 	public ClienteLogin(int cliente, int pass) {
 		usuario = cliente;
 		contrasena = pass;
 	}
+	
+	 
 	private int verificacion() {
 		for(int i = 0; i < numCliente.length; i++) {
 			if (numCliente[i] == usuario) {
@@ -24,14 +29,23 @@ public class ClienteLogin{
 				}
 			}
 		return 0;
-		}		
+		}	
+	
+	   
 	@Override
 	public String toString() {
         if (verificacion() == 0){
             return "Los datos ingresados son incorrectos.";
         }else{
-		return "Bienvenido clliente, " + usuario + "." + "\n------------------------\n----ELIJA UNA OPCION----\n-1.- Solicitud de saldo-"
-				+ "\n-2.- Retiro de dinero---\n-3.- Depósito de dinero-\n------------------------";
+		return "Bienvenido cliente, " + usuario + "." + "\n------------------------\n----ELIJA UNA OPCION----\n-1.- Solicitud de saldo-"
+				+ "\n-2.- Retiro de dinero-\n-3.- Depósito de dinero-\n-4.- Salir del sistema-\n------------------------";
         }
+        
 	}
-}
+	
+
+	
+	
+	
+	}
+
