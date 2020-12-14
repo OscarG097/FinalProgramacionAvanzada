b4e7a289c7dd5245aaa1e7caaf230d917b38c7dd
Lucas Basualdo <basualdo1995@gmail.com>
2020-12-14 16:51:12 -0300

    Modificacion en BancoPrueba.java

--- src/testBanco/BancoPrueba.java
+++ src/testBanco/BancoPrueba.java
@@ -1,12 +1,62 @@
 package testBanco;
 
+import banco.ABM;
 import banco.ClienteLogin;
+import banco.Cuentas;
+
 import java.util.Scanner;
 
 public class BancoPrueba {
 	
 	public static void main(String[] args) {
 	
+		Cuentas c1 = new Cuentas(12345,12345,40000);
+		Cuentas c2 = new Cuentas(4567,4567,45000);
+		
+		//Logeo del cliente, ingresa cuenta y nip
+		Scanner input = new Scanner(System.in);
+		System.out.println("¡BIENVENIDO!" + "\nIngrese su número de cuenta:");
+		//Ingresa Cuenta y pone valor en el constructor
+		int user = input.nextInt();
+		System.out.println("Introduzca su NIP:");
+		//Ingresa NIP y pone valor en el cosntructor
+		int contr = input.nextInt();
+		ABM cliente1 = new ABM(user, contr);
+		cliente1.addAccount(c1);
+		cliente1.addAccount(c2);
+		System.out.println(cliente1.toString());
+		
+		//Ingresar opcion 
+		System.out.println("Ingrese el numero de su seleccion:");
+		int opcion=input.nextInt();
+		//cliente1.setOpera(opcion);
+		//cliente1.operaciones(opcion);
+		//System.out.print(cliente1.operaciones(opcion));
+		
+		/*System.out.println("Ingrese cuanto desea retirar de su cuenta");
+		int retiro=input.nextInt();
+		System.out.println(cliente1.retiroDinero(retiro));
+		*/
+		
+		if(opcion != 2 & opcion !=3){
+			System.out.print(cliente1.operaciones(opcion));
+		}else if(opcion == 2) {
+			System.out.println("Ingrese cuanto desea retirar de su cuenta");
+			int retiro=input.nextInt();
+			System.out.println(cliente1.retiroDinero(retiro));
+		}else if(opcion == 3) {
+			System.out.println("Cuanto desea depositar");
+			int deposito=input.nextInt();
+			System.out.println(cliente1.depositoDinero(deposito));
+		}
+		
+		
+		
+		System.out.println("--------------------------------------------------------");
+		
+	/*Cuentas c1= new Cuentas(123456,12345,45000);	
+	c1.setSaldo(45000);
+		
 	//--> CLIENTE LOGIN <--//
 	//Logeo del cliente, ingresa cuenta y nip
 	Scanner input = new Scanner(System.in);
@@ -18,6 +68,8 @@ public class BancoPrueba {
 	int contr = input.nextInt();
 	ClienteLogin cliente1 = new ClienteLogin(user, contr);
 	System.out.println(cliente1);
+	
+	*/
 
 	}
 }
